45afef1e87955dd8386c10ae776949eeab6fea0c
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2024-07-20 15:34:44 +0900

    feat: 로그인, 회원가입 기능 구현

--- apps/client/src/components/JoinInput/JoinInput.tsx
+++ apps/client/src/components/JoinInput/JoinInput.tsx
@@ -1,33 +1,85 @@
-
+import { ChangeEvent, useState } from "react";
 import "./JoinInput.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const JoinInput = () => {
 
-    const onClickImg = () => {
-        //사진에 value를 부여해서 해당 사진의 value를 서버에 전송하도록 해야한다. 
-        
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [img, setImg] = useState(1);
+    const [link, setLink] = useState("");
+    const [introduction, setIntroduction] = useState("");
+
+    const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
     }
 
+    const onChangePwd = (e: ChangeEvent<HTMLInputElement>) => {
+        setPwd(e.target.value);
+    }
+
+    const onChangeNickname = (e: ChangeEvent<HTMLInputElement>) => {
+        setNickname(e.target.value);
+    }
+
+    const onChangeLink = (e: ChangeEvent<HTMLInputElement>) => {
+        setLink(e.target.value);
+    }
+
+    const onChangeIntroduction = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setIntroduction(e.target.value);
+    }
+
+    const Join = () => {
+
+        console.log(email + pwd + nickname + img + link + introduction);
+
+        axios.post('http://localhost:5173/api/join',
+            {
+                "email": email,
+                "pwd": pwd,
+                "nickname": nickname,
+                "img": img,
+                "link": link,
+                "introduction": introduction
+            }, {
+                headers: { 'Content-type': 'application/json' }
+            })
+        .then((res)=>{
+              console.log(res);
+              alert("회원가입 성공");
+              navigate("/login");
+          }).catch(error=>{
+              alert("회원가입 실패");
+              navigate("/");
+              console.log(error);
+              throw new Error(error);
+          });
+    }
 
     return (
         <div className="JoinInput">
             <div className="email">
                 <p>이메일</p>
                 <div style={{display: "flex"}}>
-                    <input className="emailInput" type="email" placeholder="email@hotsix.co.kr"/>
+                    <input className="emailInput" type="email" placeholder="email@hotsix.co.kr" onChange={onChangeEmail}/>
                     <button className="dupeCheck">중복 확인</button>
                 </div>
             </div>
             
             <div className="pwd">
                 <p>비밀번호</p>
-                <input className="pwdInput" type="password" placeholder="8~16자리/영문 대소문자, 숫자, 특수문자 조합"/>
+                <input className="pwdInput" type="password" placeholder="8~16자리/영문 대소문자, 숫자, 특수문자 조합" onChange={onChangePwd}/>
             </div>
             
             <div className="nickname">
                 <p>닉네임</p>
                 <div style={{display: "flex"}}>
-                    <input className="nicknameInput"/>
+                    <input className="nicknameInput" placeholder="nickname" onChange={onChangeNickname}/>
                     <button className="dupeCheck">중복 확인</button>
                 </div>
             </div>
@@ -35,7 +87,7 @@ const JoinInput = () => {
             <div className="profileImg">
                 <p>프로필 이미지</p>
                 <div style={{display: "flex", justifyContent: "space-around"}}>
-                    <img  onClick={onClickImg} src="https://picsum.photos/id/16/200/300"/>
+                    <img src="https://picsum.photos/id/16/200/300"/>
                     <img src="https://picsum.photos/id/17/200/300"/>
                     <img src="https://picsum.photos/id/29/200/300"/>
                 </div>
@@ -43,15 +95,15 @@ const JoinInput = () => {
             
             <div className="gitLink">
                 <p>Github 링크</p>
-                <input className="gitLinkInput"/>
+                <input className="gitLinkInput" placeholder="https://github.com" onChange={onChangeLink}/>
             </div>
             
             <div className="intro">
                 <p>소개</p>
-                <textarea rows={5} />
+                <textarea rows={5} placeholder="자기 자신을 간단하게 소개해주세요." onChange={onChangeIntroduction}/>
             </div>
 
-            <button className="joinBtn">회원 가입</button>
+            <button className="joinBtn" onClick={Join}>회원 가입</button>
 
         </div>
     );
--- apps/client/src/components/LoginInput/LoginInput.tsx
+++ apps/client/src/components/LoginInput/LoginInput.tsx
@@ -1,41 +1,63 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import "./LoginInput.css";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginInput = () => {
 
-    const [id, setId] = useState("");
-    const [pwd, setPwd] = useState("");
+  const navigate = useNavigate();
 
-    const onChangeId = (e) => {
-        setId(e.target.value);
-    }
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    const onChangePwd = (e) => {
-        setPwd(e.target.value);
-    }
+  const onChangeId = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-    const Login = () => {
-        axios.post("/login", {
-            
-        })
-    }
+  const onChangePwd = (e: ChangeEvent<HTMLInputElement>) => {
+    setPwd(e.target.value);
+  };
 
+  const Login = () => {
 
-    return (
-        <form>
-            <div className="LoginInput">
-                <input className="idpw" placeholder="이메일" onChange={onChangeId}/>
-                <input className="idpw" placeholder="비밀번호" type="password" onChange={onChangePwd}/>
-                <button className="loginButton" type="submit" onClick={Login}>로그인</button>
-                <a href="/">
-                    비밀번호를 잊으셨나요?
-                </a>
-            </div>
-        </form>
-        
-    );
-    
-}
+    axios({
+      method:"POST",
+      url: 'http://http://localhost:5173/api/login',
+      data:{
+          "email": email,
+          "password": pwd
+      }
+    }).then((res)=>{
+        console.log(res);
+        const token = res.data.token;
+        localStorage.setItem('jwtToken', token);
+        alert("로그인 성공");
+        navigate("/");
+    }).catch(error=>{
+        alert("로그인 실패");
+        navigate("/");
+        console.log(error);
+        throw new Error(error);
+    });
+  }
+
+  return (
+    <>
+      <div className="LoginInput">
+        <input className="idpw" placeholder="이메일" onChange={onChangeId} />
+        <input
+          className="idpw"
+          placeholder="비밀번호"
+          type="password"
+          onChange={onChangePwd}
+        />
+        <button className="loginButton" type="submit" onClick={Login}>
+          로그인
+        </button>
+        <a href="/">비밀번호를 잊으셨나요?</a>
+      </div>
+    </>
+  );
+};
 
 export default LoginInput;
