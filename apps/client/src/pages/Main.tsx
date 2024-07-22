d67951e918e65bd126554cfc065975c954dbaa4c
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-07-22 19:53:22 +0900

    feat: 메인 페이지에서 게시글 API 호출 추가

--- apps/client/src/pages/Main.tsx
+++ apps/client/src/pages/Main.tsx
@@ -1,27 +1,42 @@
+import { useEffect } from "react";
 import Profile from "../components/Profile/Profile";
 import Gallery from "../components/Gallery/Gallery";
+import postAPI from "../services/post";
 
 function Main() {
+  useEffect(() => {
+    const postArticle = async () => {
+      try {
+        const response = await postAPI.getArticles();
+        console.log("SUCCESS", response);
+      } catch (err) {
+        console.log("ERR", err.response.data);
+      }
+    };
+
+    postArticle();
+  }, []);
+
   const posts = [
     {
       id: 1,
-      author: "userName1",
+      nickname: "userName1",
       thumb: "https://picsum.photos/id/88/300/250",
       title: "First Post",
       description: "Content of the first post",
       createdAt: "2024-06-28T12:34:56.789Z",
       updatedAt: "2024-06-28T12:34:56.789Z",
-      showStatus: true,
+      status: true,
     },
     {
       id: 2,
-      author: "userName2",
+      nickname: "userName2",
       thumb: "https://picsum.photos/id/192/300/250",
       title: "Second Post",
       description: "Content of the second post",
       createdAt: "2024-06-28T12:34:56.789Z",
       updatedAt: "2024-06-28T12:34:56.789Z",
-      showStatus: true,
+      status: true,
     },
   ];
   return (
