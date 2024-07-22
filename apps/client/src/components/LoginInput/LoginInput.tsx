e54cee6845d15249a447c39cc790b0ebe143d1d5
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-07-22 14:58:45 +0900

    Feat: axios 전역 설정 및 로그인, 로그아웃 구현

--- apps/client/src/components/LoginInput/LoginInput.tsx
+++ apps/client/src/components/LoginInput/LoginInput.tsx
@@ -1,7 +1,11 @@
 import { ChangeEvent, useState } from "react";
 import "./LoginInput.css";
+import axios from "../../config/axios";
+import { setCookie } from "../../utils/cookies";
+import { useNavigate } from "react-router-dom";
 
 const LoginInput = () => {
+  const navigate = useNavigate();
   const [id, setId] = useState("");
   const [pwd, setPwd] = useState("");
 
@@ -11,6 +15,28 @@ const LoginInput = () => {
 
   const onChangePwd = (e: ChangeEvent<HTMLInputElement>) => {
     setPwd(e.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const data = {
+        email: id,
+        password: pwd,
+      };
+      const response = await axios.post("/auth/signin", data);
+      console.log("SUCCESS", response.data);
+      const accessToken = response.data.accessToken;
+      setCookie("accessToken", accessToken, {
+        path: "/",
+        // expires:
+        // secure: true, // HTTPS에서만 전송
+        // httpOnly: true
+      });
+      navigate("/");
+    } catch (err) {
+      console.log("ERR", err);
+    }
   };
 
   return (
@@ -23,7 +49,7 @@ const LoginInput = () => {
           type="password"
           onChange={onChangePwd}
         />
-        <button className="loginButton" type="submit">
+        <button className="loginButton" type="submit" onClick={handleSubmit}>
           로그인
         </button>
         <a href="/">비밀번호를 잊으셨나요?</a>
