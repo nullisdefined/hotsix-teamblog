3cd3e23a05370220776411c3200d9ec9aca164de
nullisdefined <jaeuu.dev@gmail.com>
2024-07-26 08:19:45 +0900

    Style: HtmlRenderer MarkDown

--- apps/client/src/components/Post/HtmlRenderer.tsx
+++ apps/client/src/components/Post/HtmlRenderer.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import DOMPurify from "dompurify";
 import hljs from "highlight.js";
 import "highlight.js/styles/github.css";
-import "./HtmlRenderer.css"; // 새로운 CSS 파일을 만들어 스타일을 정의합니다
+import "./HtmlRenderer.css";
 
 interface Props {
   htmlContent: string;
@@ -13,9 +13,28 @@ const HtmlRenderer: React.FC<Props> = ({ htmlContent }) => {
 
   useEffect(() => {
     if (containerRef.current) {
+      // 코드 블록 하이라이팅
       const codeBlocks = containerRef.current.querySelectorAll("pre code");
       codeBlocks.forEach((block) => {
         hljs.highlightElement(block as HTMLElement);
+      });
+
+      // 순서 없는 리스트 스타일 적용
+      const ulElements = containerRef.current.querySelectorAll("ul");
+      ulElements.forEach((ul) => {
+        ul.classList.add("md-list", "md-list-disc");
+      });
+
+      // 순서 있는 리스트 스타일 적용
+      const olElements = containerRef.current.querySelectorAll("ol");
+      olElements.forEach((ol) => {
+        ol.classList.add("md-list", "md-list-decimal");
+      });
+
+      // 테이블 스타일 적용
+      const tableElements = containerRef.current.querySelectorAll("table");
+      tableElements.forEach((table) => {
+        table.classList.add("md-table");
       });
     }
   }, [htmlContent]);
@@ -25,7 +44,7 @@ const HtmlRenderer: React.FC<Props> = ({ htmlContent }) => {
   return (
     <div
       ref={containerRef}
-      className="html-content"
+      className="md-content"
       dangerouslySetInnerHTML={{ __html: cleanHtml }}
     />
   );
